695c6e7f0cc96ceae930598ad3e4087f1fff33a9
中田誠人 <masato.nakata1250softbank.com@gmail.com>
2023-01-29 18:09:00 +0900

    カテゴリ一覧作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,30 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to 
+    end
+  end
+
   def search
     @categories = Category.where(kind: params[:kind])
     respond_to do |format|
       format.json { render json: @categories }
     end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
